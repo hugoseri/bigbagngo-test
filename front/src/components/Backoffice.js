04010bfc9f977021d01b1c8525a3d2600a8fbffa
hugoseri <hserieys@enovacom.fr>
2021-07-04 18:14:05 +0200

    Add modal layout

--- front/src/components/Backoffice.js
+++ front/src/components/Backoffice.js
@@ -3,8 +3,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import TextField from "@material-ui/core/TextField";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CloseIcon from "@material-ui/icons/Close";
 
 import ShopApi from "api/shops-api";
 import NoResult from "components/NoResult";
@@ -47,20 +53,88 @@ const useStyles = makeStyles((theme) => ({
   shopText: {
     fontWeight: ({ header }) => header && 600,
   },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    minWidth: 500,
+  },
+  field: {
+    margin: theme.spacing(1, 2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
 }));
 
 export default function Backoffice() {
   const classes = useStyles();
+  const [openModal, setOpenModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState("Add a shop");
+
   return (
     <div className={classes.root}>
+      <Modal open={openModal} setOpen={setOpenModal} title={modalTitle} />
       <div className={classes.header}>
         <Typography variant="h5">Shops manager</Typography>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setOpenModal(true)}
+        >
           Add a shop
         </Button>
       </div>
       <ShopList />
     </div>
+  );
+}
+
+function Modal({ open, setOpen, title, name, description, location }) {
+  const classes = useStyles();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="Add or update shop modal"
+    >
+      <DialogTitle>
+        {title}
+        <IconButton
+          onClick={() => setOpen(false)}
+          className={classes.closeButton}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <form className={classes.form}>
+          <TextField
+            label="Name"
+            variant="outlined"
+            className={classes.field}
+          ></TextField>
+          <TextField
+            label="Description"
+            variant="outlined"
+            className={classes.field}
+          ></TextField>
+          <TextField
+            label="Location"
+            variant="outlined"
+            className={classes.field}
+          ></TextField>
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
@@ -75,7 +149,7 @@ function ShopList() {
       .then((result) => {
         setShopList(result);
       })
-      .finally(() => setLoading(true));
+      .finally(() => setLoading(false));
   }, []);
 
   if (loading) {
@@ -93,7 +167,11 @@ function ShopList() {
   return (
     <div className={classes.shopList}>
       {shopList && <Shop shop={{ id: "ID", name: "Name" }} header />}
-      {shopList ? shopList.map((shop) => <Shop shop={shop} />) : <NoResult />}
+      {shopList ? (
+        shopList.map((shop) => <Shop key={shop.name} shop={shop} />)
+      ) : (
+        <NoResult />
+      )}
     </div>
   );
 }
