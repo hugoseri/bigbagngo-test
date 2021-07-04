04cf03c38d5510d3bbb720400c0d44aed13f5695
hugoseri <hserieys@enovacom.fr>
2021-07-04 17:00:38 +0200

    Add 404 page styling

--- front/src/components/Shops.js
+++ front/src/components/Shops.js
@@ -3,10 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { ReactComponent as NoResultIllustration } from "assets/sad.svg";
 import { Link } from "react-router-dom";
 
 import ShopApi from "api/shops-api";
+import { ReactComponent as NoResultIllustration } from "assets/sad.svg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
