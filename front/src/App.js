3f9fd57809e74fe3be18da71101b2e4b5fa691d8
hugoseri <hserieys@enovacom.fr>
2021-07-04 14:53:11 +0200

    Add material UI theme

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { lightBlue } from "@material-ui/core/colors";
 
 import NavBar from "components/NavBar";
 import Shops from "components/Shops";
 import Backoffice from "components/Backoffice";
 import NoMatch from "components/NoMatch";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightBlue[500],
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Shops />
-        </Route>
-        <Route path="/admin">
-          <Backoffice />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Shops />
+          </Route>
+          <Route path="/admin">
+            <Backoffice />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
