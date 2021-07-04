dc725d0b3a569582c9d2663ff5a2377d1c7fcfb0
hugoseri <hserieys@enovacom.fr>
2021-07-04 15:25:09 +0200

    Add navbar styling

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
 import { lightBlue } from "@material-ui/core/colors";
 
 import NavBar from "components/NavBar";
 import Shops from "components/Shops";
 import Backoffice from "components/Backoffice";
 import NoMatch from "components/NoMatch";
+import "index.css";
 
 const theme = createMuiTheme({
   palette: {
@@ -14,26 +19,39 @@ const theme = createMuiTheme({
       main: lightBlue[500],
     },
   },
+  typography: {
+    fontFamily: "Quicksand",
+  },
 });
 
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "100vh",
+    width: "100vw",
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route exact path="/">
-            <Shops />
-          </Route>
-          <Route path="/admin">
-            <Backoffice />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <div className={classes.root}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <Shops />
+            </Route>
+            <Route path="/admin">
+              <Backoffice />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </div>
   );
 }
 
--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -1,5 +1,53 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import BallIcon from "@material-ui/icons/SportsFootball";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.primary.dark,
+    color: theme.palette.common.white,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: theme.spacing(3, 6),
+    justifyContent: "space-between",
+  },
+  home: {
+    display: "flex",
+    alignItems: "center",
+    "& svg": {
+      paddingRight: theme.spacing(2),
+    },
+  },
+  button: {
+    color: theme.palette.common.white,
+  },
+}));
 
 export default function NavBar() {
-  return <div>This is a navbar.</div>;
+  const classes = useStyles();
+
+  return (
+    <AppBar position="sticky" className={classes.root}>
+      <Link to="/" className={classes.home}>
+        <BallIcon fontSize="large" />
+        <Typography variant="h5" className={classes.title}>
+          Sport center
+        </Typography>
+      </Link>
+      <Link to="/admin">
+        <Button
+          variant="text"
+          startIcon={<SettingsIcon />}
+          className={classes.button}
+        >
+          Manage shops
+        </Button>
+      </Link>
+    </AppBar>
+  );
 }
