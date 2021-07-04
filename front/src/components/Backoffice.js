1bfab82185716d185f04a055e275983727a39ca7
hugoseri <hserieys@enovacom.fr>
2021-07-04 17:10:09 +0200

    Add basic backoffice list

--- front/src/components/Backoffice.js
+++ front/src/components/Backoffice.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import ShopApi from "api/shops-api";
 
 export default function Backoffice() {
-  return <div>This is the backoffice page !</div>;
+  const [shopList, setShopList] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    ShopApi.getAll()
+      .then((result) => {
+        setShopList(result);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div>
+      {shopList ? (
+        shopList.map((shop) => <div>{shop.name}</div>)
+      ) : (
+        <div>Pas de résultat</div>
+      )}
+    </div>
+  );
 }
