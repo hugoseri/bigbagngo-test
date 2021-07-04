f2d2ac36ea64ec5fd2b8f817e1c139a5755348b0
hugoseri <hserieys@enovacom.fr>
2021-07-04 17:22:41 +0200

    Add style to backoffice list

--- front/src/components/Backoffice.js
+++ front/src/components/Backoffice.js
@@ -1,8 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import ShopApi from "api/shops-api";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    margin: "auto",
+    maxWidth: 1200,
+    padding: theme.spacing(6),
+  },
+  shop: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderTop: ({ header }) =>
+      !header && `solid 0.5px ${theme.palette.divider}`,
+    padding: theme.spacing(2),
+  },
+  shopText: {
+    fontWeight: ({ header }) => header && 600,
+  },
+}));
+
 export default function Backoffice() {
+  const classes = useStyles();
   const [shopList, setShopList] = useState();
   const [loading, setLoading] = useState();
 
@@ -16,11 +44,29 @@ export default function Backoffice() {
   }, []);
 
   return (
-    <div>
+    <div className={classes.root}>
+      <Shop shop={{ id: "ID", name: "Name" }} header />
       {shopList ? (
-        shopList.map((shop) => <div>{shop.name}</div>)
+        shopList.map((shop) => <Shop shop={shop} />)
       ) : (
         <div>Pas de résultat</div>
+      )}
+    </div>
+  );
+}
+
+function Shop({ shop, header }) {
+  const classes = useStyles({ header });
+  return (
+    <div className={classes.shop}>
+      <Typography className={classes.shopText}>{shop.id}</Typography>
+      <Typography className={classes.shopText}>{shop.name}</Typography>
+      {!header ? (
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      ) : (
+        <Typography className={classes.shopText}>Actions</Typography>
       )}
     </div>
   );
